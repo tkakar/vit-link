7a257e148d1a0573bc75bf31a878a8dc2677b6ed
Tabassum Kakar <tabassum_kakar@hms.harvard.edu>
2024-10-03 11:19:41 -0400

    testing code

---
+++ src/components/StudyIdInput/StudyIdInput.tsx
@@ -0,0 +1,47 @@
+import React, { ChangeEvent } from "react";
+import styles from "../ConfigEditor/ConfigEditor.module.css";
+import { STUDY_ID_LENGTH } from "../../utils/constants.ts";
+
+interface StudyIdInputProps {
+	onInputError: (errorMessage: string | null) => void;
+	onInputChange: (value: string) => void;
+}
+
+export const StudyIdInput: React.FC<StudyIdInputProps> = ({
+	onInputError,
+	onInputChange,
+}) => {
+	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const { value } = e.target;
+		if (value.trim()?.length === STUDY_ID_LENGTH) {
+			if (!/^\d+$/.test(value)) {
+				onInputError("Team ID must be numbers only");
+			} else {
+				onInputChange(value);
+				onInputError(null);
+			}
+		} else {
+			onInputError(`Team ID must be ${STUDY_ID_LENGTH} digits`);
+		}
+	};
+
+	return (
+		<>
+			<label
+				className={`${styles.viewConfigInputUrlOrFileText} ${styles.requiredField}`}
+				htmlFor="inputField"
+			>
+				{" "}
+				Enter your Team ID
+			</label>
+			<input
+				type="text"
+				id="inputField"
+				className={styles.viewConfigUrlInput}
+				placeholder={`${STUDY_ID_LENGTH}-Digit Id`}
+				onChange={handleInputChange}
+				maxLength={STUDY_ID_LENGTH}
+			/>
+		</>
+	);
+};
---
+++ src/utils/utility-functions.ts
@@ -0,0 +1,57 @@
+import { upgradeAndParse } from "@vitessce/schemas";
+import { ERROR_MESSAGES } from "./constants";
+import { Config } from "./config-examples";
+
+export const validateConfig = (
+	nextConfig: string,
+): [boolean, string | null] => {
+	let upgradeSuccess: boolean;
+	let failureReason: Config | any = null;
+	try {
+		failureReason = upgradeAndParse(JSON.parse(nextConfig));
+		upgradeSuccess = true;
+	} catch (e: any) {
+		upgradeSuccess = false;
+		failureReason = e.message;
+		console.error(e);
+	}
+	return [upgradeSuccess, failureReason];
+};
+
+export const sanitizeURLs = (urls: string) => {
+	return urls
+		.split(/;/)
+		.map((url) => url.trim())
+		.filter((url) => url.match(/^http/g));
+};
+
+// Only 1 dataset url is allowed at the moment
+export const updateConfigWithExampleURL = (config: Config, urls: string[]) => {
+	if (urls.length === 1) {
+		config.datasets[0].files[0].url = urls[0];
+		config.datasets[0].files[0].options = {
+			offsetsUrl: urls[0].split(".ome.")[0] + ".offsets.json",
+		};
+	}
+	return config;
+};
+
+export const studyIdFetcher = async (url: string) => {
+	try {
+		const response = await fetch(url);
+
+		if (response.status === 403) {
+			throw new Error(ERROR_MESSAGES.FORBIDDEN_STUDY_ID);
+		}
+
+		if (!response.ok) {
+			throw new Error("Could not fetch Link ID");
+		}
+
+		return await response.json();
+	} catch (error) {
+		throw error;
+	}
+};
+
+export const fetcher = (url: string) => fetch(url).then((res) => res.json());
