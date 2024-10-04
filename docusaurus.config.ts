a717a00ed7487a114994d6c7529712a4d3427844
Tabassum Kakar <tabassum_kakar@hms.harvard.edu>
2024-10-03 20:34:10 -0400

    undoing the change

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: process.env.VITE_VITESSCE_DOCS_BASE_URL,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Vitessce', // Usually your GitHub org/user name.
